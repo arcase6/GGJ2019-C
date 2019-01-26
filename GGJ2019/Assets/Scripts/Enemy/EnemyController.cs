66db9a8b7191ed809236a49ba5dd74662629cd66
arcase6 <41893575+arcase6@users.noreply.github.com>
2019-01-27 08:55:17 +0900

    Refined Player movement

---
+++ GGJ2019/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyController : MonoBehaviour
+{
+    public Vector2 TargetLocation;
+    public Mover mover;
+    public bool targetReached = false;
+    public Rigidbody2D rb;
+    public Vector2 PatrolPoint1;
+    public Vector2 PatrolPoint2;
+    public float DefaultPatrolOffset = 20;
+    [HideInInspector]
+    public Transform playerTransform;
+    
+
+    public void Reset()
+    {
+        mover = GetComponent<Mover>();
+        rb = GetComponent<Rigidbody2D>();
+        targetReached = false;
+        PatrolPoint1 = new Vector2(rb.position.x - DefaultPatrolOffset,rb.position.y);
+        PatrolPoint2 = new Vector2(rb.position.x + DefaultPatrolOffset,rb.position.y);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TargetLocation = PatrolPoint1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //check to see if target reached
+        if (!targetReached && TargetLocation != null)
+        {
+            float difference = TargetLocation.x - rb.position.x;
+            mover.ApplyHorizontalMovement(difference);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
---
+++ GGJ2019/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Mover mover;
+    public Rigidbody2D rb;
+    public int maxJumps = 2;
+    private int jumpsRemaining;
+    [HideInInspector]
+    public List<Interactable> availableInteractables = new List<Interactable>();
+    [HideInInspector]
+    public bool isBusyWithAnimation;
+    [HideInInspector]
+    public bool isHiding;
+    public BoxCollider2D PlayerHeart;
+    public CapsuleCollider2D MainCollider;
+    private Interactable previousInteractable;
+    private float normalGravity;
+
+    public void SetVisibilty(bool visibility)
+    {
+        PlayerHeart.enabled = visibility;
+        
+    }
+
+    public void SetHiding(bool isHiding)
+    {
+        this.isHiding = isHiding;
+        if (isHiding)
+        {
+            rb.velocity = new Vector2(0, 0);
+            rb.gravityScale = 0;
+            MainCollider.enabled = false;
+        }
+        else
+        {
+            rb.gravityScale = normalGravity;
+            MainCollider.enabled = true;
+        }
+    }
+
+    private void Reset()
+    {
+        mover = GetComponent<Mover>();
+        rb = GetComponent<Rigidbody2D>();
+        PlayerHeart = GetComponentInChildren<BoxCollider2D>();
+        MainCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpsRemaining = maxJumps;
+        mover.OnGroundedChanged.AddListener(RefillJumps);
+        normalGravity = rb.gravityScale;
+    }
+
+    void RefillJumps()
+    {
+        if (mover.grounded && rb.velocity.y < 2)
+            jumpsRemaining = maxJumps;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && (availableInteractables.Count > 0 || isHiding))
+        {
+            if (!isHiding)
+            {
+                previousInteractable = availableInteractables.First();
+                availableInteractables.First().InteractWith(this);
+            }
+            else
+                previousInteractable.InteractWith(this);
+            return;
+        }
+        if (!this.isHiding)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            if (horizontalInput != 0)
+            {
+                mover.ApplyHorizontalMovement(horizontalInput);
+            }
+            
+            if (Input.GetButtonDown("Jump") && jumpsRemaining > 0)
+            {
+                jumpsRemaining--;
+                mover.Jump();
+                mover.grounded = false;
+            }
+            else if (Input.GetButtonUp("Jump"))
+            {
+                if (rb.velocity.y > 0)
+                    mover.ReduceJump();              
+            }
+        }
+        
+    }
+}
