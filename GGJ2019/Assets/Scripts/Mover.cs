ab21d3eadc044a6b42a0d82a7c3e3b3b68c55135
arcase6 <41893575+arcase6@users.noreply.github.com>
2019-01-26 14:31:29 +0900

    Sample Scene

---
+++ GGJ2019/Assets/Scripts/Mover.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float MaxSpeed = 5;
+    public int Acceleration = 1;
+
+
+    private void Reset()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        MaxSpeed = 50;
+        Acceleration = 10;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if (horizontalInput != 0)
+        {
+            Vector2 acceleration = new Vector2(Acceleration, 0);
+            Vector2 currentSpeed = rb.velocity;
+            //allow acceleration in the opposite direction
+            if (horizontalInput < 0)
+            {
+                if (currentSpeed.x < MaxSpeed * -1)
+                    return;
+                acceleration *= -1;
+            }
+            else
+            {
+                if (currentSpeed.x > MaxSpeed)
+                    return; 
+            }
+
+            acceleration = acceleration * Time.deltaTime;
+            ApplyAcceleration(acceleration);
+        }
+    }
+
+    public void ApplyAcceleration(Vector2 acceleration)
+    {
+        rb.velocity += acceleration;
+
+    }
+}
