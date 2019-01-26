6281f779104bf7e86846f244a2cb272df50bd251
arcase6 <41893575+arcase6@users.noreply.github.com>
2019-01-26 16:25:59 +0900

    Basic Movement functionality

---
+++ GGJ2019/Assets/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool grounded = true;
+    public Mover mover;
+    public Rigidbody2D rb;
+
+    private void Reset()
+    {
+        mover = GetComponent<Mover>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Detect collision with floor
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        Debug.Log("Hit detected");
+        if (hit.gameObject.tag == "Floor")
+        {
+            grounded = true;
+        }
+    }
+
+    // Detect collision exit with floor
+    void OnCollisionExit2D(Collision2D hit)
+    {
+        Debug.Log("Leave detected");
+
+        if (hit.gameObject.tag == "Floor")
+        {
+            grounded = false;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grounded = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if (horizontalInput != 0)
+        {
+            mover.ApplyHorizontalMovement(horizontalInput);
+        }
+
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            mover.Jump();
+            grounded = false;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (rb.velocity.y > 0)
+            {
+                mover.ReduceJump();
+            }
+        }
+    }
+}
--- GGJ2019/Assets/Scripts/Mover.cs
+++ GGJ2019/Assets/Scripts/Mover.cs
@@ -7,13 +7,14 @@ public class Mover : MonoBehaviour
     public Rigidbody2D rb;
     public float MaxSpeed = 5;
     public int Acceleration = 1;
-
+    public float jumpTakeOffSpeed = 7;
 
     private void Reset()
     {
         rb = GetComponent<Rigidbody2D>();
         MaxSpeed = 50;
         Acceleration = 10;
+        jumpTakeOffSpeed = 7;
     }
 
 
@@ -30,27 +31,43 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        if (horizontalInput != 0)
-        {
-            Vector2 acceleration = new Vector2(Acceleration, 0);
-            Vector2 currentSpeed = rb.velocity;
-            //allow acceleration in the opposite direction
-            if (horizontalInput < 0)
-            {
-                if (currentSpeed.x < MaxSpeed * -1)
-                    return;
-                acceleration *= -1;
-            }
-            else
-            {
-                if (currentSpeed.x > MaxSpeed)
-                    return; 
-            }
+        
+    }
 
-            acceleration = acceleration * Time.deltaTime;
-            ApplyAcceleration(acceleration);
+    public void ReduceJump()
+    {
+        Vector2 newSpeed = rb.velocity;
+        newSpeed.y = jumpTakeOffSpeed / 2;
+        if(rb.velocity.y > newSpeed.y)
+        rb.velocity = newSpeed;
+    }
+
+    public void Jump()
+    {
+        Vector2 newSpeed = rb.velocity;
+        newSpeed.y = jumpTakeOffSpeed;
+        rb.velocity = newSpeed;
+    }
+
+    public void ApplyHorizontalMovement(float horizontalInput)
+    {
+        Vector2 acceleration = new Vector2(Acceleration, 0);
+        Vector2 currentSpeed = rb.velocity;
+        //allow acceleration in the opposite direction
+        if (horizontalInput < 0)
+        {
+            if (currentSpeed.x <= MaxSpeed * -1)
+                return;
+            acceleration *= -1;
         }
+        else
+        {
+            if (currentSpeed.x >= MaxSpeed)
+                return;
+        }
+
+        acceleration = acceleration * Time.deltaTime;
+        ApplyAcceleration(acceleration);
     }
 
     public void ApplyAcceleration(Vector2 acceleration)
