4d51438637e3bc1040a4ba14ca5e6c1f6f1cb927
arcase6 <41893575+arcase6@users.noreply.github.com>
2019-01-27 02:06:35 +0900

    Fixed Jumping

--- GGJ2019/Assets/Scripts/Mover.cs
+++ GGJ2019/Assets/Scripts/Mover.cs
@@ -8,8 +8,11 @@ public class Mover : MonoBehaviour
     public Rigidbody2D rb;
     public float MaxSpeed = 5;
     public int Acceleration = 1;
+    public int Decceleration = 100;
     public float jumpTakeOffSpeed = 7;
     public bool grounded = true;
+    public Animator MovementAnimator;
+    public Transform feet;
 
     private void Reset()
     {
@@ -17,6 +20,8 @@ public class Mover : MonoBehaviour
         MaxSpeed = 25;
         Acceleration = 25;
         jumpTakeOffSpeed = 7;
+        MovementAnimator = GetComponentInChildren<Animator>();
+        feet = GetComponentsInChildren<Transform>().Where(t => t.gameObject.name == "Feet").FirstOrDefault();
     }
 
 
@@ -32,14 +37,42 @@ public class Mover : MonoBehaviour
     // Update is called once per 
     void Update()
     {
+        bool almostStopped = rb.velocity.x < 1 && rb.velocity.x > -1;
+        if (Input.GetAxis("Horizontal") == 0 && gameObject.tag == "Player" && grounded && !almostStopped)
+        {
+            Vector2 decceleration = new Vector2(Decceleration * Time.deltaTime, 0);
+            if (rb.velocity.x > 0)
+                decceleration = decceleration * -1;
+            ApplyAcceleration(decceleration);
+        }
+
+
         for (int i = -1; i <= 1; i += 2)
         {
-            grounded = Physics2D.RaycastAll(new Vector2(transform.position.x + i / 2, transform.position.y), new Vector2(0, -1.0f), 1.0f)
+            grounded = Physics2D.RaycastAll(new Vector2(feet.position.x + i / 2, feet.position.y), new Vector2(0, -1.0f), 1.0f)
                 .Where(h => h.collider != null)
                 .Any(h => h.collider.gameObject.tag == "Floor");
             if (grounded)
                 break;
         }
+        MovementAnimator.SetBool("Ground", grounded);
+
+        float velocity = rb.velocity.x;
+
+        float moveBlend = Mathf.Abs(rb.velocity.x) / (MaxSpeed);
+        if (moveBlend < 0) moveBlend = 0;
+        if (moveBlend > 1) moveBlend = 1;
+
+        MovementAnimator.SetFloat("velocity",velocity);
+        MovementAnimator.SetFloat("moveBlend", moveBlend);
+        Vector3 oldScale = MovementAnimator.transform.localScale;
+        Vector3 newScale = new Vector3(Mathf.Abs(oldScale.x), oldScale.y, oldScale.z);
+        if (velocity > 1)
+            newScale.x = newScale.x * -1;
+        if(!almostStopped)
+        MovementAnimator.transform.localScale = newScale;
+        float verticalSpeed = Mathf.Abs(rb.velocity.y);
+        MovementAnimator.SetFloat("verticalSpeed", verticalSpeed);
     }
 
     public void ReduceJump()
@@ -55,6 +88,7 @@ public class Mover : MonoBehaviour
         Vector2 newSpeed = rb.velocity;
         newSpeed.y = jumpTakeOffSpeed;
         rb.velocity = newSpeed;
+        MovementAnimator.SetTrigger("Jump");
     }
 
     public void ApplyHorizontalMovement(float horizontalInput)
