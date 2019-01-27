449cabf195431848377e9d1e4382e7a4108b19d5
dentsu-ict <dentsu.ict@gmail.com>
2019-01-27 09:15:53 +0900

    title
    画面

---
+++ GGJ2019/Assets/Resources/Scripte/FadeScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FadeScript : MonoBehaviour {
+
+    public Image images;
+
+    void Awake()
+    {
+        images = GetComponent<Image>();
+    }
+
+    public void FadeIn()
+    {
+        if (images.enabled) iTween.ValueTo(gameObject, iTween.Hash("from", 0f, "to", 1f, "time", 2f, "onupdate", "SetValue"));
+    }
+    public void FadeOut()
+    {
+        if(images.enabled) iTween.ValueTo(gameObject, iTween.Hash("from", 1f, "to", 0f, "time", 2f, "onupdate", "SetValue"));
+    }
+    public void FadeSet(float alpha, float nowalpha)
+    {
+        if (images.enabled) iTween.ValueTo(gameObject, iTween.Hash("from", nowalpha, "to", alpha, "time", 2f, "onupdate", "SetValue"));
+    }
+    public void DemoFadeOut()
+    {
+        if (images.enabled) iTween.ValueTo(gameObject, iTween.Hash("from", 1f, "to", 0f, "time", 2f, "onupdate", "SetValue"));
+    }
+    void SetValue(float alpha)
+    {
+        images.color = new Color(0, 0, 0, alpha);
+    }
+    public void SetEnable()
+    {
+        images.enabled = true;
+    }
+    public void SetDisable()
+    {
+        images.enabled = false;
+    }
+}
---
+++ GGJ2019/Assets/Resources/Scripte/Title.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public GameObject fadeobj;
+    float time = 2.0f;
+    int flag = 0;
+    FadeScript fade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fade = fadeobj.GetComponent<FadeScript>();
+        fade.FadeOut();
+        flag = 1;
+        time = 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            if (time <= 0)
+            {
+                time = 0;
+                if (flag == 1)
+                {
+                    fade.gameObject.SetActive(false);
+                    flag = 0;
+                }
+                if (flag == 2)
+                {
+                    loadScene();
+
+                }
+
+            }
+        }
+        if(Input.GetButtonDown("Jump")){
+            OnClick();
+
+        }
+    }
+
+    public void OnClick()
+    {
+        if (flag == 0)
+        {
+            flag = 2;
+            fade.gameObject.SetActive(true);
+            time = 2.0f;
+            fade.FadeIn();
+        }
+    }
+    public void loadScene()
+    {
+        SceneManager.LoadScene("stage1"); ;
+    }
+}
